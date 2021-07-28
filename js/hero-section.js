54b58bdde1a3dcb0eeb2b6d34a69636e770209da
lorenzoSpec <john.erwin.lorenzo.2002@gmail.com>
2021-07-28 19:25:08 +0800

    added snapping featuer on different sections

--- js/hero-section.js
+++ js/hero-section.js
@@ -1,32 +1,17 @@
 function animationHero() {
   const IMG = document.getElementById('img-hero');
   const TITLE = document.getElementById('title-cont');
-  let screenWidth = window.innerWidth;
 
-  decideWhatClass(screenWidth, IMG, TITLE);
+  decideWhatClass(IMG, TITLE);
 }
 
-function decideWhatClass(screenWidth, IMG, TITLE){
-  if(screenWidth >= 360 && screenWidth < 1280){
-    animationImgOne(IMG);
-    animationTitleOne(TITLE);
-    console.log('sj');
-  } else {
+function decideWhatClass(IMG, TITLE){
     animationImgTwo(IMG);
     animationTitleTwo(TITLE);
-  }
-}
-
-function animationImgOne(IMG) {
-  IMG.setAttribute('class', 'animate-hero');
 }
 
 function animationImgTwo(IMG) {
   IMG.setAttribute('class', 'animate-hero-two');
-}
-
-function animationTitleOne(TITLE) {
-  TITLE.setAttribute('class', 'animate-hero-title');
 }
 
 function animationTitleTwo(TITLE) {
